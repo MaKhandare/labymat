aefaee4a01e04fadee4c749b427b069207b5541a
MaKhandare <khandare.matthaeus@gmail.com>
2023-02-25 06:44:07 +0100

    init commit

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -8,18 +8,69 @@ public class Grid : MonoBehaviour {
     [SerializeField] private GameObject nodePrefab;
     [SerializeField] private Transform cam;
 
-    private const int WIDTH = 5;
-    private const int HEIGHT = 5;
-    private GameObject[,] grid = new GameObject[WIDTH, HEIGHT];
+    private const int WIDTH = 25;
+    private const int HEIGHT = 25;
+    public GameObject[,] grid = new GameObject[WIDTH, HEIGHT];
+
+    Node current;
+    Stack mazeGeneratorStack;
 
 
     void Start() {
-
         CreateGrid();
-
+        StartCoroutine("MazeGenerator");
     }
 
-    void Update() {
+
+    /*         
+
+        https://en.wikipedia.org/wiki/Maze_generation_algorithm
+
+        1. Make the initial cell the current cell and mark it as visited
+        2. While there are unvisited cells
+            1. If the current cell has any neighbours which have not been visited
+                1. Choose randomly one of the unvisited neighbours
+                2. Push the current cell to the stack
+                3. Remove the wall between the current cell and the chosen cell
+                4. Make the chosen cell the current cell and mark it as visited
+            2. Else if stack is not empty
+                1. Pop a cell from the stack
+                2. Make it the current cell 
+    */
+    IEnumerator MazeGenerator() {
+
+        mazeGeneratorStack = new Stack();
+
+        current = grid[0, 0].GetComponent<Node>();
+        current.SetVisited(true);
+        current.SetStartColor();
+
+        while (UnvisitedNodesLeft()) {
+
+            current.SetVisitedColor();
+            current.SetVisited(true);
+
+            List<Node> unvisitedNeighbors = current.GetUnvisitedNeighbors();
+
+            if (unvisitedNeighbors.Count > 0) {
+                Node next = unvisitedNeighbors[Random.Range(0, unvisitedNeighbors.Count)];
+                mazeGeneratorStack.Push((Node)current);
+
+                current.removeWalls(next);
+
+                next.SetVisited(true);
+                current = next;
+            } else if (mazeGeneratorStack.Count > 0) {
+                current = (Node)mazeGeneratorStack.Pop();
+                current.SetStackColor();
+            }
+
+            yield return new WaitForSeconds(.01f);
+        }
+
+        current.SetVisitedColor();
+        grid[0, 0].GetComponent<Node>().SetStartColor();
+        grid[WIDTH - 1, HEIGHT - 1].GetComponent<Node>().SetEndColor();
 
     }
 
@@ -29,8 +80,7 @@ public class Grid : MonoBehaviour {
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
                 GameObject node = Instantiate(nodePrefab, new Vector3(x, y), Quaternion.identity);
-
-                node.name = $"Node: {x} {y}";
+                node.name = $"{x},{y}";
                 node.transform.SetParent(this.transform);
 
                 grid[x, y] = node;
@@ -38,7 +88,27 @@ public class Grid : MonoBehaviour {
         }
 
 
+        // add neighbors for each node in grid after creating it
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                grid[x, y].GetComponent<Node>().GetNeighbors(grid, x, y);
+            }
+        }
+
+
         // adjust camera position
         cam.transform.position = new Vector3((float)WIDTH / 2 - 0.5f, (float)HEIGHT / 2 - 0.5f, -10);
+    }
+
+
+    private bool UnvisitedNodesLeft() {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (!grid[x, y].GetComponent<Node>().GetVisited() == true) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,18 +4,118 @@ using UnityEngine;
 
 public class Node : MonoBehaviour {
 
+    [SerializeField] private Color baseColor, visitedColor, stackColor, startColor, endColor;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    private bool visited;
+    public List<Node> neighbors;
+
     public GameObject wallTop;
     public GameObject wallRight;
     public GameObject wallBottom;
     public GameObject wallLeft;
 
+    public void SetStartColor() {
+        spriteRenderer.color = startColor;
+    }
 
-    // private void Start() {
+    public void SetEndColor() {
+        spriteRenderer.color = endColor;
+    }
 
-    //     // to disable walls or destroy the walls
-    //     // think about which to choose
-    //     // wallTop.SetActive(false);
-    //     // GameObject.Destroy(wallTop);
-    // }
+    public void SetVisitedColor() {
+        spriteRenderer.color = visitedColor;
+    }
+
+    public void SetStackColor() {
+        spriteRenderer.color = stackColor;
+    }
+
+    public void SetVisited(bool x) {
+        visited = x;
+    }
+
+    public bool GetVisited() {
+        return visited;
+    }
+
+    public void removeWalls(Node other) {
+        int x = (int)(this.transform.position.x - other.transform.position.x);
+        int y = (int)(this.transform.position.y - other.transform.position.y);
+
+        if (x == 1) {
+            this.wallLeft.SetActive(false);
+            other.wallRight.SetActive(false);
+        } else if (x == -1) {
+            this.wallRight.SetActive(false);
+            other.wallLeft.SetActive(false);
+        }
+
+
+        if (y == 1) {
+            this.wallBottom.SetActive(false);
+            other.wallTop.SetActive(false);
+        } else if (y == -1) {
+            this.wallTop.SetActive(false);
+            other.wallBottom.SetActive(false);
+        }
+    }
+
+    public void GetNeighbors(GameObject[,] grid, int x, int y) {
+        neighbors = new List<Node>();
+
+
+        // top neighbor
+        try {
+            neighbors.Add(grid[x, y + 1].GetComponent<Node>());
+        } catch {
+            // Debug.Log("no top neighbor");
+        }
+
+        // right neighbor
+        try {
+            neighbors.Add(grid[x + 1, y].GetComponent<Node>());
+        } catch {
+            // Debug.Log("no right neighbor");
+        }
+
+
+        // bottom neighbor
+        try {
+            neighbors.Add(grid[x, y - 1].GetComponent<Node>());
+        } catch {
+            // Debug.Log("no bottom neighbor");
+        }
+
+        // left neighbor
+        try {
+            neighbors.Add(grid[x - 1, y].GetComponent<Node>());
+        } catch {
+            // Debug.Log("no left neighbor");
+        }
+
+    }
+
+    public Node GetRandomNeighborNode() {
+        if (neighbors.Count > 0) {
+
+            return neighbors[Random.Range(0, neighbors.Count)];
+        }
+
+        return null;
+    }
+
+    public List<Node> GetUnvisitedNeighbors() {
+        List<Node> unvisitedNeighbors = new List<Node>();
+
+        for (int i = 0; i < neighbors.Count; i++) {
+            if (!neighbors[i].GetVisited()) {
+                unvisitedNeighbors.Add(neighbors[i]);
+            }
+        }
+
+        return unvisitedNeighbors;
+    }
+
+
 
 }
